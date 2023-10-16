43ade708444e28497bd645056a1353338bda3bef
USMAN-AHMAD-SHEHU <ahmadshehu.057@gmail.com>
2023-10-16 12:44:14 +0100

    ignore

--- jsBasics.js
+++ jsBasics.js
@@ -1,16 +1,1 @@
-// CHALLENGE: GRADE CALCULATOR challenges
-
-var mark = prompt('Input your grade');
-var grade;
-if(mark >= 70 && mark <= 100){
-    alert('Grade: A');
-}
-else if(mark >= 50 && mark <= 60){
-    alert('Grade: C');
-}
-else if(mark >= 0 && mark <=40){
-    alert('Grade: F');
-}
-else{
-    alert('Mark is invalid');
-}
+//Please ignore
